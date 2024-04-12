903e88909916798db494eefb32657ed018d3c3bd
janatr <janatrpkovska0@gmail.com>
2024-04-12 22:43:28 +0200

    Поправки во методите filterOddLengthStrings и makeBigStringFrom

--- SILab1_1.java
+++ SILab1_1.java
@@ -1,17 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
-public class SILab1Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-
-        List<String> list = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            list.add(sc.next());
+public class SILab1_1 {
+    public static List<String> filterOddLengthStrings(List<String> list) {
+        List<String> result = new ArrayList<>();
+        for (String el : list) {
+            if (el.length() % 2 != 0) {
+                result.add(el);
+            }
         }
+        return result;
     }
+
+
 }
